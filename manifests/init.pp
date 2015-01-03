de1fed84bbdaa961dc5e14fe44ce221bc1194590
Tom McLaughlin <tmclaughlin@hubspot.com>
2015-01-02 22:06:02 -0500

    Add basic rhel package support

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,8 @@
 # [user] The user that should own that directory (default: www-data)
 # [servers] An array of servers (default: [])
 # [revision] The revision for the deployment (default: 'origin/REL_5-1')
+# [use_package] Use OS package instead of Git. (default: false)
+# [package_ensure] 'ensure' value passed to package resource if use_package is true. (default: installed)
 #
 # === Examples
 #
@@ -39,24 +41,41 @@ class phppgadmin (
   $path = '/srv/phppgadmin',
   $user = 'www-data',
   $servers = [],
-  $revision = 'origin/REL_5-1'
+  $revision = 'origin/REL_5-1',
+  $use_package = false,
+  $package_ensure = installed
 ) {
-  file { $path:
-    ensure         => directory,
-    owner          => $user,
+
+  if $use_package == true {
+    package { 'phpPgAdmin':
+      ensure => $package_ensure
+    }
+
+    $phppgadmin_conf_require = Package['phpPgAdmin']
+    $phppgadmin_conf_path    = '/etc/phpPgAdmin/config.inc.php'
+  } else {
+    file { $path:
+      ensure         => directory,
+      owner          => $user,
+    }
+    ->
+    vcsrepo { $path:
+      ensure   => present,
+      provider => git,
+      source   => 'git://github.com/phppgadmin/phppgadmin.git',
+      user     => $user,
+      revision => $revision,
+    }
+
+    $phppgadmin_conf_require = Vcsrepo[$path]
+    $phppgadmin_conf_path    = "${path}/conf/config.inc.php"
   }
-  ->
-  vcsrepo { $path:
-    ensure   => present,
-    provider => git,
-    source   => 'git://github.com/phppgadmin/phppgadmin.git',
-    user     => $user,
-    revision => $revision,
-  }
-  ->
+
   file { 'phppgadmin-conf':
-    path    => "${path}/conf/config.inc.php",
+    path    => $phppgadmin_conf_path,
     content => template('phppgadmin/config.inc.php.erb'),
     owner   => $user,
+    require => $phppgadmin_conf_require
   }
+
 } # Class:: phppgadmin
